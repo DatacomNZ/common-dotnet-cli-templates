ced0e9d751514cbea75781a7d4cfd8c7b11e036e
Niall Duane <n_duane@hotmail.com>
2019-09-11 19:48:21 +1200

    Updated Nuget.config file. Updated Package.ps1 file Added azure-pipelines yaml to pavlova contents.

--- src/Pavlova/Contents/Package.ps1
+++ src/Pavlova/Contents/Package.ps1
@@ -1,1 +1,4 @@
-dotnet pack
+param (
+    [string]$outputdirectory = "./output"
+)
+dotnet publish ".\src\4. Presentation\Pav.Lova.Presentation.Api\Pav.Lova.Presentation.Api.csproj" -c Release --output "$outputdirectory/Pav.Lova.Presentation.Api"
