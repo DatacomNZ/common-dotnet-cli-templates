adca90077773e93a5267939e431b3be26b55eb35
Niall Duane <n_duane@hotmail.com>
2019-09-20 10:47:22 +1200

    Fixed issue on Package.ps script where it is not finding the output directory if relative path.

--- src/CheeseCake/Contents/Package.ps1
+++ src/CheeseCake/Contents/Package.ps1
@@ -3,6 +3,7 @@ param (
 )
 
 if ([System.IO.Path]::IsPathRooted($outputdirectory) -eq $false) {
+    New-Item -ItemType Directory -Force -Path $outputdirectory
     $outputdirectory = Resolve-Path $outputdirectory
 }
 dotnet publish ".\src\4. Presentation\Pav.Lova.Presentation.Api\Pav.Lova.Presentation.Api.csproj" -c Release --output "$outputdirectory/Pav.Lova.Presentation.Api"
